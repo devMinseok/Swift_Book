b4967331a0f945d2018432224b41eb32bba9fc7d
KangMinseok <qbq5000@gmail.com>
2020-01-26 23:51:34 +0900

    add: <Set> + Set문법을 추가하였습니다.

---
+++ Swift_Book.playground/Pages/Set.xcplaygroundpage/Contents.swift
@@ -0,0 +1,17 @@
+
+import UIKit
+
+// 검색 속도가 더 중요할 경우 주로 사용
+
+let set: Set<Int> = [1, 2, 2, 3, 3, 3]
+set.count
+
+// 요소의 자료형은 생략 가능
+let set2: Set = [1, 1, 2, 2, 3, 3]
+
+// MARK: Inspecting a Set
+
+set2.count
+
+set2.isEmpty
+
